6b09ac3bfc19688595afcf62493eb0cc60fda83e
Josue Anes-Paz <JosueRAnes@proton.me>
2023-04-11 11:53:38 -0400

    Add flash messages
    Added success and error messages. Made messages dismissible.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -38,12 +38,20 @@ router.post('/', validateCampground, catchAsync(async (req, res) => {
 // Show Specific Campground Route
 router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground) {
+        req.flash('error', 'Campground not found!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
 // Edit Specific Campground Route
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
+    if(!campground) {
+        req.flash('error', 'Campground not found!');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
 }));
 
@@ -51,6 +59,7 @@ router.get('/:id/edit', catchAsync(async (req, res) => {
 router.put('/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`)
 }));
 
@@ -58,6 +67,7 @@ router.put('/:id', validateCampground, catchAsync(async (req, res) => {
 router.delete('/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground!')
     res.redirect('/campgrounds');
 }));
 
