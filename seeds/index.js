d470032a3172e12afe648b48e1f814681f8de3e2
Josue Anes-Paz <JosueRAnes@proton.me>
2023-04-13 12:38:39 -0400

    Update Models
    Added on the author string to the schemas.

--- seeds/index.js
+++ seeds/index.js
@@ -1,34 +1,36 @@
-const mongoose = require('mongoose');
-const cities = require('./cities');
-const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for(let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20 + 10);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fuga voluptatibus atque, nisi voluptates autem voluptate maiores deserunt cupiditate incidunt, optio animi sit nesciunt excepturi laudantium veritatis numquam quidem magnam',
-            price
-        })
-        await camp.save();
-    }
-}
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20 + 10);
+    const camp = new Campground({
+      author: "643813884ff289593642ff7c",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fuga voluptatibus atque, nisi voluptates autem voluptate maiores deserunt cupiditate incidunt, optio animi sit nesciunt excepturi laudantium veritatis numquam quidem magnam",
+      price,
+    });
+    await camp.save();
+  }
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
-})
+  mongoose.connection.close();
+});
