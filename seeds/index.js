33cd3a852fe10083c337afc510474b75363870b2
Josue Anes-Paz <JosueRAnes@proton.me>
2023-04-15 13:51:25 -0400

    Update Map
    Added geometry location and cluster data for campgrounds.

--- seeds/index.js
+++ seeds/index.js
@@ -15,16 +15,24 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20 + 10);
     const camp = new Campground({
+      // Your user ID
       author: "643813884ff289593642ff7c",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum fuga voluptatibus atque, nisi voluptates autem voluptate maiores deserunt cupiditate incidunt, optio animi sit nesciunt excepturi laudantium veritatis numquam quidem magnam",
       price,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       images: [
         {
           url: "https://res.cloudinary.com/dklufe3jc/image/upload/v1681487399/YelpCamp/dxi3ifadnyahsiyyoww4.jpg",
